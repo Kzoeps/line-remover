fb792974c44c1c97e451c750b64c097870de3130
kzoeps <kzoepa@gmail.com>
2023-11-02 15:38:57 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import { Button, ChakraProvider, Flex, Textarea, useClipboard } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [original, setOriginal] = useState<string>('')
+  const { onCopy, setValue, hasCopied } = useClipboard('')
+
+  const removeLines = (text: string) => {
+    return text.replace(/(\r\n|\n|\r)/gm, '')
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setOriginal(e.target.value)
+    setValue(removeLines(e.target.value))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ChakraProvider>
+        <Flex direction={"column"} gap={5}>
+          <Textarea onChange={handleChange}  value={original} rows={8} />
+          <Button variant={'outline'} colorScheme='blue'onClick={() => setOriginal('')}>Reset</Button>
+          <Textarea onChange={handleChange} value={removeLines(original)} rows={8} />
+          <Button variant={'outline'} colorScheme='blue' onClick={onCopy}>{hasCopied ? 'Copied' : 'Copy'}</Button>
+        </Flex>
+      </ChakraProvider>
     </>
   )
 }
